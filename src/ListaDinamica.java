91ccb3dbce7af74ce92a5d7d6f32b2d0ee5814d3
Desvelados <DesveladosD@gmail.com>
2015-10-16 10:25:42 -0300

    First

---
+++ src/ListaDinamica.java
@@ -0,0 +1,7 @@
+
+
+/**
+ * Created by bb185046 on 16/10/2015.
+ */
+public class ListaDinamica implements Lista {
+}
---
+++ src/Nodo.java
@@ -0,0 +1,17 @@
+import java.util.Objects;
+
+/**
+ * Created by bb185046 on 16/10/2015.
+ */
+public class Nodo
+{
+    private Object dato;
+    private Nodo prox;
+
+
+    public Nodo (Object x, Nodo p)
+    {
+        dato=x;
+        prox=p;
+    }
+}
