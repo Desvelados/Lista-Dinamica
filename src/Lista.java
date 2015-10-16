0f86d0e9ea1c1be0db3126d98816ac41342bfe65
Desvelados <DesveladosD@gmail.com>
2015-10-16 10:57:13 -0300

    Todas las clases
    a

---
+++ src/Lista.java
@@ -0,0 +1,21 @@
+/**
+ * Created by bb185046 on 16/10/2015.
+ */
+public interface Lista {
+
+
+        void insertarDespues(Object x);
+        void insertarAntes(Object x);
+        void eliminar();
+        void irSiguiente();
+        void irAnterior();
+        void irA (int n);
+        Object verActual();
+        int verPosActual();
+        int longitud();
+        boolean esVacio();
+        boolean finLista();
+    }
+
+
+
--- src/ListaDinamica.java
+++ src/ListaDinamica.java
@@ -1,7 +1,28 @@
-
-
 /**
  * Created by bb185046 on 16/10/2015.
  */
-public class ListaDinamica implements Lista {
+public class ListaDinamica  {
+
+    private Nodo com;
+    private Nodo ult;
+    private Nodo actual;
+    private int cantidad;
+
+    public ListaDinamica()
+    {
+        com=new Nodo("cabecera",null);
+        ult=actual=com;
+        cantidad=0;
+
+    }
+    public boolean esVacia()
+    {
+        return cantidad==0;
+
+    }
+  /*  public void irPrimero()
+    {
+        actual=com.getProx();
+    }
+    */
 }
